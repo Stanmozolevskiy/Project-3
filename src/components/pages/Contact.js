0cb8efb4e8ef434e6405b75988183d217e8bc096
Orlando Correa <orlandocorrea12@yahoo.com>
2019-02-21 21:22:58 -0500

    added api fuctionality

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,25 +1,14 @@
-import React from "react";
-import NavTabs from "../NavTabs"
-
-function Contact() {
-  return (
-    <div>
-      {<NavTabs />}
-      <h1>Blog Page</h1>
-      <p>
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, 
-        totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
-        dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,
-         sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
-          est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam 
-          eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad
-           minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex
-            ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse
-             quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur
-      </p>
-    </div>
-  );
-}
-
-export default Contact;
-
+const axios = require('axios');
+require('dotenv').config();
+const id = "4fd1d55d"
+const key = "9788c6b183e3995fbc144d4b1b300850"
+const search = "Steak";
+axios.get("https://api.nutritionix.com/v1_1/search/" + search + "?results=0:20&fields=item_name,brand_name,item_id,nf_calories&appId=" + id + "&appKey=" + key)
+  .then(response => {
+    const itemName = response.data.hits[0].fields.item_name
+    const brand = response.data.hits[0].fields.brand_name
+    const calories = response.data.hits[0].fields.nf_calories
+    const servingSize = response.data.hits[0].fields.nf_serving_size_qty
+    console.log(response.data.hits[0].fields)
+    console.log("\nName: " + itemName + "\nBrand: " + brand + "\nCalories: " + calories + "\nServing Size: " + servingSize)
+  });
