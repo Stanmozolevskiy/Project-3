fcaf91f62afce9f431744724b539c02d06f6a892
Sam Kfoury <you@example.com>
2019-02-26 20:59:59 -0500

    added header and footer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import About from "./components/pages/About";
 import Blog from "./components/pages/Blog";
 import Contact from "./components/pages/Contact";
-import Login from "./components/pages/login page/Login"
+import Login from "./components/pages/login page/Login";
+import CustomNavbar from "./components/Navbar";
+import CustomFooter from "./components/Footer";
+import './appstyle.css';
 
 
 
@@ -11,8 +14,9 @@ function App() {
   return (
     <Router>
       <div>
-        
+        <CustomNavbar />
         <Login />
+        <CustomFooter />
         <Route exact path="/about" component={About}  />
         <Route exact path="/blog" component={Blog} />
         <Route exact path="/contact" component={Contact} />
