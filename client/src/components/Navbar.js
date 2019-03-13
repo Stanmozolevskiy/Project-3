62e7379ae1ba348721150789991813742bfea27e
Orlando Correa <orlandocorrea12@yahoo.com>
2019-03-12 21:22:57 -0400

    some style changes

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -9,26 +9,26 @@ import homeimage from './Image/home.png';
 
 class CustomNavbar extends React.Component {
 
-render() {
-  return (
-    <Navbar expand="lg" id="navbar">
-    <Navbar fixed="top" />
-    <div class="col-md-2 text-center"> <Link to=""><Image src={homeimage} /></Link> </div>
-      <div class="col-md-3">
-      <Link to="FitnessLog"><Button size="large" className="btn-outline-secondary col-md-12" id="navbutton">Fitness Log</Button></Link>
-      </div>
-      <div class="col-md-3">
-      <Link to="FitnessSummary" ><Button size="large" className="btn-outline-secondary col-md-12" id="navbutton">Fitness Summary</Button></Link>
-      </div>
-      <div class="col-md-2">
-      <Link to="/myProfile"> <Button size="large" className="btn-outline-secondary col-md-12"  id="navbutton">Profile</Button> </Link>
-      </div>
-    <div class="col-md-2">
-      <Link to="/SignIn"><Button size="large" className="btn-outline-secondary col-md-12" id="navbutton">Sign In</Button></Link>
-      </div>
-    </Navbar>
-  );
-}
+  render() {
+    return (
+      <Navbar expand="lg" id="navbar">
+        <Navbar fixed="top" />
+        <div class="col-md-2 text-center"> <Link to=""><Image src={homeimage} /></Link> </div>
+        <div class="col-md-3">
+          <Link to="FitnessLog"><Button size="large" className="btn-outline-secondary col-md-12" id="navbutton">Fitness Log</Button></Link>
+        </div>
+        <div class="col-md-3">
+          <Link to="FitnessSummary" ><Button size="large" className="btn-outline-secondary col-md-12" id="navbutton">Fitness Summary</Button></Link>
+        </div>
+        <div class="col-md-2">
+          <Link to="/myProfile"> <Button size="large" className="btn-outline-secondary col-md-12" id="navbutton">Profile</Button> </Link>
+        </div>
+        <div class="col-md-2">
+          <Link to="/SignIn"><Button size="large" className="btn-outline-secondary col-md-12" id="navbutton">Sign In</Button></Link>
+        </div>
+      </Navbar>
+    );
+  }
 }
 
 export default CustomNavbar;
