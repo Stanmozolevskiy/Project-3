a4e88facef542b370eef847b00d56e21e3f1b623
Scott <smcanally11@yahoo.com>
2019-03-16 19:56:39 -0400

    Add about me page  after user signs up, add card for profile on sign in page

--- client/src/components/pages/MyProfile.js
+++ client/src/components/pages/MyProfile.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'whatwg-fetch';
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Container, Row, Col, Image } from "react-bootstrap";
 import { getFromStorage, } from '../../utils/storage';
 import API from "../../utils/API";
 import {Redirect} from 'react-router-dom'
@@ -255,11 +255,53 @@ class User extends React.Component {
         }
 
         return (
+            <Container>
             <div>
-                <p>Account Details </p>
-                
-                <Button variant="light" onClick={this.logout}>Logout</Button>
+                <h1>Thank you for signing up for Fitness First!</h1>
+                <br />
+                <h2>About the site </h2>
+                <Row>
+                    <Col>
+                <p>Fitness first is designed to be a simple, easy to use web application, to help users stay on track and achieve their fitness goals</p>
+                    </Col>
+                </Row>
+                <h3>Home </h3>
+                <Row>
+                    <Col>
+                        <Image src="https://images.unsplash.com/photo-1480074568708-e7b720bb3f09?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2853&q=80" fluid/>
+
+                    </Col>
+                <Col>
+                <p>The app features a home button in the top left hand corner. Within home, the user can enter in the exercise and duration of activity. This easy to use
+                    search box will then return the estimated calories burned from the physical activity. Listed below, is another search box where the user can enter in
+                    any meal or snack they cosumed throughout the day. A listing of cards will appear with information of the food such as calories, manufacturer and serving size. </p>
+                    </Col>
+                </Row>
+                <br />
+                <h3>Fitness Log </h3>
+                <Row>
+                    <Col>
+                        <Image src="https://images.unsplash.com/1/irish-hands.jpg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80" fluid />
+                    </Col>
+                    <Col>
+                <p>The fitness log page features two tables. One for keeping track of the exercises you complete on a day to day basis. The other table is designed to monitor each meal
+                    of the day based on your total calorie consumption. The results the user searches for on the home page can be used within either log.
+                </p>
+                    </Col>
+                </Row>
+                <br />
+                <h3>Fitness Summary </h3>
+                <Row>
+                    <Col> 
+                        <Image src="https://images.unsplash.com/photo-1543286386-713bdd548da4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80" fluid />
+                    </Col>
+                <Col><p>Last but not least is the fitness summary page. This page is designed to give the user an easy to depict graph of their progress. As the user adds, deletes or updates rows from the tables, the charts will adjust accordingly.</p> </Col>
+                </Row>
+                <br />
+                <br />
+                {/* <Button variant="light" onClick={this.logout}>Logout</Button> */}
             </div>
+            </Container>
         );
     }
 }
